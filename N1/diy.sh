18e2fe0b736cd0cce0f6b3c0c95ba00b6eb130a5
AJ <983097239@qq.com>
2024-10-04 07:56:33 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -16,23 +16,7 @@ git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-alidd
 
 # => openclash
 git clone https://github.com/vernesong/OpenClash.git --depth 1 package/openclash
-# 检查是否成功克隆，并列出文件
-if [ -d "package/openclash/luci-app-openclash" ]; then
-    echo "OpenClash cloned successfully."
-    ls package/openclash/luci-app-openclash  # 列出文件以确认 Makefile 的存在
-else
-    echo "Failed to clone OpenClash."
-    exit 1
-fi
 
-# 查看 Makefile
-if [ -f "package/openclash/luci-app-openclash/Makefile" ]; then
-    cat package/openclash/luci-app-openclash/Makefile
-else
-    echo "Makefile does not exist."
-fi
-
-
-#主题
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+# => mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
